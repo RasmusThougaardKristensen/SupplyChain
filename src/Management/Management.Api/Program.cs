9a02eb8d515568e390918a813f170c63348deb67
Rasmus T Kristensen <rtk@umbraco.dk>
2025-09-25 12:30:42 +0200

    Api setup

--- src/Management/Management.Api/Program.cs
+++ src/Management/Management.Api/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace SupplyChain.Management.Api;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SupplyChain.Management.Application;
+using SupplyChain.Management.Infrastructure;
+
+namespace SupplyChain.Management.Api;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+        builder.Services.AddSwaggerGen();
+
+
+        builder.Services.AddRepositories();
+
+        builder.Services.AddApplicationServices();
+        builder.Services.AddUseCases();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.RoutePrefix = string.Empty;
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        await app.RunAsync();
     }
 }
