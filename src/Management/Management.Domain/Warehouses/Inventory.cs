ffeeeab0354370e16d791da84fb18fd7d61a8ca3
Rasmus T Kristensen <rtk@umbraco.dk>
2025-09-27 12:20:28 +0200

    Business logic to increase and decrease stock level for inventory

--- src/Management/Management.Domain/Warehouses/Inventory.cs
+++ src/Management/Management.Domain/Warehouses/Inventory.cs
@@ -1,6 +1,6 @@
 using SupplyChain.Management.Domain.Warehouses.Stocks;
 
-namespace SupplyChain.Management.Domain.Warehouse;
+namespace SupplyChain.Management.Domain.Warehouses;
 
 public sealed record Inventory
 {
@@ -10,5 +10,29 @@ public sealed record Inventory
     public Inventory(List<Stock> stocks)
     {
         _stocks = stocks;
+    }
+
+    public void IncreaseStockLevel(Stock increaseStock)
+    {
+        var existingStock = _stocks.FirstOrDefault(stock => stock.Sku.Id == increaseStock.Sku.Id);
+
+        if (existingStock == null)
+        {
+            throw new InvalidOperationException($"Stock with SKU '{increaseStock.Sku.Id}' not found in warehouse inventory");
+        }
+
+        existingStock.IncreaseQuantity(increaseStock.Quantity);
+    }
+
+    public void DecreaseStockLevel(Stock decreaseStock)
+    {
+        var existingStock = _stocks.FirstOrDefault(stock => stock.Sku.Id == decreaseStock.Sku.Id);
+
+        if (existingStock == null)
+        {
+            throw new InvalidOperationException($"Stock with SKU '{decreaseStock.Sku.Id}' not found in warehouse inventory");
+        }
+
+        existingStock.DecreaseQuantity(decreaseStock.Quantity);
     }
 }
