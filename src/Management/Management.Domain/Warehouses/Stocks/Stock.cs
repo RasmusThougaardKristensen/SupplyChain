b56527bd27ce5c104b849dd73913537d9fac14dd
Rasmus T Kristensen <rtk@umbraco.dk>
2025-09-27 12:20:00 +0200

    Business method to increase quantity of stock

--- src/Management/Management.Domain/Warehouses/Stocks/Stock.cs
+++ src/Management/Management.Domain/Warehouses/Stocks/Stock.cs
@@ -4,12 +4,17 @@ namespace SupplyChain.Management.Domain.Warehouses.Stocks;
 
 public sealed record Stock
 {
-
     public Sku Sku { get; }
-    public int Quantity { get; }
+    public int Quantity { get; private set; }
     public DateTime DeliveryDate { get; } //TODO: Figure out if stock model should include delivery date of the stock?
     public Uom Uom { get; }
     public StockPlacement Placement { get; }
+
+    public Stock(Sku sku, int quantity)
+    {
+        Sku = sku;
+        Quantity = quantity;
+    }
 
     public Stock(Sku sku, int quantity, DateTime deliveryDate, Uom uom, StockPlacement placement)
     {
@@ -18,5 +23,16 @@ public sealed record Stock
         DeliveryDate = deliveryDate;
         Uom = uom;
         Placement = placement;
+    }
+
+    public void IncreaseQuantity(int increaseQuantity) => Quantity += increaseQuantity;
+    public void DecreaseQuantity(int decreaseQuantity)
+    {
+        if (Quantity < decreaseQuantity)
+        {
+            throw new InvalidOperationException($"Insufficient stock for SKU {Sku.Id}. Available stock: {Quantity}");
+        }
+
+        Quantity -= decreaseQuantity;
     }
 }
