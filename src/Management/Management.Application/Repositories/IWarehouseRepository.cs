f70f6e0cc9aedc463e960d0066743a189138984d
Rasmus T Kristensen <rtk@umbraco.dk>
2025-09-30 19:47:30 +0200

    Moved + refactored method of getting warehouses with skus

--- src/Management/Management.Application/Repositories/IWarehouseRepository.cs
+++ src/Management/Management.Application/Repositories/IWarehouseRepository.cs
@@ -1,6 +1,9 @@
+using SupplyChain.Management.Domain.LegoSets;
+using SupplyChain.Management.Domain.Warehouses;
+
 namespace SupplyChain.Management.Application.Repositories;
 
 public interface IWarehouseRepository
 {
-
+    IReadOnlyList<WarehouseModel> GetWarehousesWithSkus(IReadOnlyList<Sku> requestedSkus);
 }
--- src/Management/Management.Infrastructure/Repositories/Warehouses/WarehousesRepository.cs
+++ src/Management/Management.Infrastructure/Repositories/Warehouses/WarehousesRepository.cs
@@ -1,6 +1,9 @@
 using System.Globalization;
 using CsvHelper;
 using SupplyChain.Management.Application.Repositories;
+using SupplyChain.Management.Domain.LegoSets;
+using SupplyChain.Management.Domain.Warehouses;
+using SupplyChain.Management.Domain.Warehouses.Stocks;
 
 namespace SupplyChain.Management.Infrastructure.Repositories.Warehouses;
 
@@ -8,6 +11,66 @@ public class WarehousesRepository : IWarehouseRepository
 {
     private const string stocksPath = "/Users/rasmuskristensen/RiderProjects/SupplyChain/src/Management/Management.Infrastructure/Repositories/Warehouses/stock.csv";
 
+
+    public IReadOnlyList<WarehouseModel> GetWarehousesWithSkus(IReadOnlyList<Sku> requestedSkus)
+    {
+        var stockEntities = ReadStocksFromCsv();
+
+        var availableStocks = stockEntities
+            .Where(stock => requestedSkus.Contains(new Sku(stock.SKU)))
+            .GroupBy(stock => stock.Warehouse)
+            .ToList();
+
+
+        var warehouses = availableStocks
+            .Select(stockInWarehouse =>
+                ToModel(stockInWarehouse, new WarehouseLocation(stockInWarehouse.Key)))
+            .ToList();
+
+        return warehouses;
+    }
+
+    private WarehouseModel ToModel(IEnumerable<StockEntity> stockEntities, WarehouseLocation warehouseLocation)
+    {
+        var stocks = new List<Stock>();
+        foreach (var stockEntity in stockEntities)
+        {
+            var stock = new Stock(
+                new Sku(stockEntity.SKU),
+                stockEntity.Quantity,
+                ParseDeliveryDate(stockEntity.DeliveryDate),
+                new Uom(stockEntity.Uom),
+                new StockPlacement(stockEntity.Placement, stockEntity.Shelf));
+
+            stocks.Add(stock);
+        }
+
+        var inventory = new Inventory(stocks);
+        var warehouse = new WarehouseModel(
+            location: warehouseLocation,
+            inventory: inventory);
+
+        return warehouse;
+    }
+
+    private DateTime ParseDeliveryDate(string dateString)
+    {
+        if (DateTime.TryParse(dateString, out DateTime result))
+        {
+            return result;
+        }
+
+        string[] formats = { "yyyy-MM-dd" };
+        foreach (var format in formats)
+        {
+            if (DateTime.TryParseExact(dateString, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out result))
+            {
+                return result;
+            }
+        }
+
+        throw new FormatException($"Unable to parse date: {dateString}");
+    }
     private static IReadOnlyList<StockEntity> ReadStocksFromCsv()
     {
         using var reader = new StringReader(File.ReadAllText(stocksPath));
