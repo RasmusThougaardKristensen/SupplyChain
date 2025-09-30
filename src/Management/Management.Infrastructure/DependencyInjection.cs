a314835d40b9d3a62480fab4f2bee4989fb4f2b7
Rasmus T Kristensen <rtk@umbraco.dk>
2025-09-30 19:46:13 +0200

    Dependency injection of repositories

--- src/Management/Management.Infrastructure/DependencyInjection.cs
+++ src/Management/Management.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using SupplyChain.Management.Application.Components;
+using SupplyChain.Management.Application.Repositories;
 using SupplyChain.Management.Infrastructure.Datasets;
+using SupplyChain.Management.Infrastructure.Repositories.LegoSets;
+using SupplyChain.Management.Infrastructure.Repositories.Warehouses;
 
 namespace SupplyChain.Management.Infrastructure;
 
@@ -14,6 +17,8 @@ public static class DependencyInjection
     public static IServiceCollection AddComponents(this IServiceCollection services)
     {
         services.AddScoped<ICsvComponent, CsvComponent>();
+        services.AddScoped<IWarehouseRepository, WarehousesRepository>();
+        services.AddScoped<ILegoSetRepository, LegoSetRepository>();
         return services;
     }
 }
